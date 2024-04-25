7fdf9109b7bddbcb84196c0f73b6a1eb27e46b35
Fernando Nicolás <he440185@uaeh.edu.mx>
2024-04-24 22:35:43 -0600

    Add the Course Microservice and Configure Eureka and Student Application.yml

--- microservice-student/src/main/java/com/microservice/student/MicroserviceStudentApplication.java
+++ microservice-student/src/main/java/com/microservice/student/MicroserviceStudentApplication.java
@@ -2,7 +2,14 @@ package com.microservice.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Porn defecto, Spring detecta que es cliente de Eureka,
+ * pero siempre es bueno tenerlo de manera explicita,
+ * por eso se usa @EnableDiscoveryClient
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceStudentApplication {
 
